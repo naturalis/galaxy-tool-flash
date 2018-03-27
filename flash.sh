882ebbe0ef02dc70e3cbf3a32ec1b0a08ab2966e
root <martenhoogeveen@gmail.com>
2018-03-27 14:28:56 +0200

    Update

--- flash.sh
+++ flash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
+#location for the production server
+outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
+#location for the testserver
+#outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
 flash_wrapper.py -i $1 -of $outlocation -t $4 -f $5 -m $6 -x $7 -M $8
 mv $outlocation"/log.log" $3
 mv $outlocation"/merged.zip" $2
