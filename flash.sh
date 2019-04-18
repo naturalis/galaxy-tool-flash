68b2e13e9c48461ad97acd9d26c5e12ef2b9fc62
dickgroenenberg <dickgroenenberg@gmail.com>
2019-04-18 13:17:42 +0000

    set outlocation to /media

--- flash.sh
+++ flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-outlocation=$(mktemp -d /home/galaxy/galaxy/database/XXXXXX)
+outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
 
 python $SCRIPTDIR"/flash_wrapper.py" -i $1 -of $outlocation -t $4 -f $5 -m $6 -x $7 -M $8
